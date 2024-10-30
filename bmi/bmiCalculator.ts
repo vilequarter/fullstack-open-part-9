9af69784f8db46ae5efed806a62defd33ec84184
vilequarter <daparady@gmail.com>
2024-10-30 13:01:30 +0200

    exercise 9.5

--- bmi/bmiCalculator.ts
+++ bmi/bmiCalculator.ts
@@ -1,3 +1,5 @@
+require.main === module;
+
 interface BmiValues {
   height: number;
   weight: number;
@@ -17,19 +19,39 @@ const parseArguments = (args: string[]): BmiValues => {
   }
 };
 
+interface BmiResponse {
+  height: number;
+  weight: number;
+  bmi: number;
+  message: string;
+}
+
 // weight / ((height / 100)^2)
 // < 18.5 = underweight
 // 18.5 - 24.9 = normal range
 // 25 - 29.9 = overweight
 // >= 30 = obese
-const calculateBmi = (height: number, weight: number): String => {
+export const calculateBmi = (height: number, weight: number): BmiResponse => {
   const bmi = weight / (Math.pow((height / 100), 2));
+  /*
   let message = `height: ${height}cm\nweight: ${weight}kg\nbmi = ${bmi}\n`;
   if(bmi < 18.5) message += 'Underweight';
   if(bmi >= 18.5 && bmi < 25) message += 'Normal Range';
   if(bmi >= 25 && bmi < 30) message += 'Overweight';
   if(bmi >= 30) message += 'Obese';
   return message;
+  */
+  let message : string = '';
+  if(bmi < 18.5) message = 'Underweight';
+  if(bmi >= 18.5 && bmi < 25) message = 'Normal Range';
+  if(bmi >= 25 && bmi < 30) message = 'Overweight';
+  if(bmi >= 30) message = 'Obese';
+  return {
+    height: height,
+    weight: weight,
+    bmi: bmi,
+    message: message 
+  };
 };
 
 try {
--- bmi/index.ts
+++ bmi/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+  if(isNaN(Number(req.query.height)) || isNaN(Number(req.query.weight))) {
+    res.status(400).json({error: 'malformatted parameters'})
+  } else {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+  
+    const message = calculateBmi(height, weight);
+    res.send(message)
+  }
+})
 
 const PORT = 3003;
 
