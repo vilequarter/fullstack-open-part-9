f29e91d9009772b97a45d013459f0b118479fa33
vilequarter <daparady@gmail.com>
2024-11-18 09:14:55 +0200

    exercise 9.20

--- flight-diaries/frontend/components/entryForm.tsx
+++ flight-diaries/frontend/components/entryForm.tsx
@@ -7,8 +7,8 @@ interface EntryFormProps {
 
 const EntryForm = (props: EntryFormProps) => {
   const [date, setDate] = useState('');
-  const [weather, setWeather] = useState('');
-  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('sunny');
+  const [visibility, setVisibility] = useState('great');
   const [comment, setComment] = useState('');
 
   const submit = async (event: React.SyntheticEvent) => {
@@ -17,8 +17,8 @@ const EntryForm = (props: EntryFormProps) => {
     props.createEntry(date, weather, visibility, comment);
 
     setDate('');
-    setWeather('');
-    setVisibility('');
+    setWeather('sunny');
+    setVisibility('great');
     setComment('');
   }
 
@@ -29,23 +29,35 @@ const EntryForm = (props: EntryFormProps) => {
         <div>
           date
           <input
+            type="date"
             value={date}
             onChange={({ target }) => setDate(target.value)}
           />
         </div>
         <div>
           weather
-          <input
+          <select
             value={weather}
             onChange={({ target }) => setWeather(target.value)}
-          />
+          >
+            <option value="sunny">sunny</option>
+            <option value="rainy">rainy</option>
+            <option value="cloudy">cloudy</option>
+            <option value="stormy">stormy</option>
+            <option value="windy">windy</option>
+          </select>
         </div>
         <div>
           visibility
-          <input
+          <select
             value={visibility}
             onChange={({ target }) => setVisibility(target.value)}
-          />
+          >
+            <option value="great">great</option>
+            <option value="good">good</option>
+            <option value="ok">ok</option>
+            <option value="poor">poor</option>
+          </select>
         </div>
         <div>
           comment
